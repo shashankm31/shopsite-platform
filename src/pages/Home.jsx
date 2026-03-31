5cf68e6babb0d4b2b876c9b4596180e6bd7b9aa7
shashankm31 <shashankm.31@outlook.com>
2026-04-01 02:30:42 +0530

    Trigger CI/CD pipeline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div style={styles.container}>
-            <h1>Build Websites. Scale Business.</h1>
+            <h1>Build Websites and Scale Business fully digitally</h1>
             <p>High-performance websites deployed on AWS cloud.</p>
         </div>
     );
